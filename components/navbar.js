1a35079588609ac6b04e4e004c26b1e3df36a2d7
Bruno Hiis <brunohiis@gmail.com>
2022-11-20 23:01:09 +0200

    Landing page & navbar ready

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,31 @@
 class navbar extends HTMLElement {
-
-    constructor (){
-        super();
-        let templateContent = `
-        <div>
-            <p>my navbar</p>
+  constructor() {
+    super()
+    let templateContent = `
+        <div class="navbar-wrapper">
+            <h3>
+            <svg style="margin-right: 8px" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd" />
+</svg>
+            Amasoon</h3>
+            <div>
+            <a href="./index.html">Homepage</a>
+            <a href="./products.html">Products</a>
+            <a href="./cart.html">Cart</a>
+            </div>
         </div>`
-        //const shadowRoot = this.attachShadow({mode: "open"})
-        //shadowRoot.innerHTML = templateContent
-        this.innerHTML = templateContent
+    //const shadowRoot = this.attachShadow({mode: "open"})
+    //shadowRoot.innerHTML = templateContent
+    this.innerHTML = templateContent
+  }
 
+  connectedCallback() {
+    console.log("connected")
+  }
 
-    }
-
-    connectedCallback(){
-        console.log("connected")
-
-    }
-
-    disconnectedCallback(){
-
-    }
-
+  disconnectedCallback() {}
 }
 
-if ('customElements' in window){
-    customElements.define('nav-bar',navbar)
+if ("customElements" in window) {
+  customElements.define("nav-bar", navbar)
 }
