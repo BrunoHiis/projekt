0af33117f84556ba75da50dc1ac75e87e8ca1f2b
Bruno Hiis <brunohiis@gmail.com>
2022-12-04 15:47:58 +0200

    fixes and improvements for products page and cart

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,15 @@
 //Raido Aunpuu
 
-function removeProductFromCart(event){
+function removeProductFromCart(event) {
   //event.target on nupp, millel vajutati
   let id = event.target.parentElement.rowIndex - 1
 
   let cartJson = JSON.parse(localStorage.getItem("cart"))
 
   //Eemalda indexi kaudu vastav element
-  cartJson.splice(id,1)
+  cartJson.splice(id, 1)
 
-  localStorage.setItem("cart",JSON.stringify(cartJson))
+  localStorage.setItem("cart", JSON.stringify(cartJson))
 
   //Eemalda kogu rea element
   event.target.parentElement.remove()
@@ -18,24 +18,24 @@ function removeProductFromCart(event){
   createSummary()
 }
 
-function createCartTable(){
-  let cartJson = JSON.parse(localStorage.getItem("cart")) 
+function createCartTable() {
+  let cartJson = JSON.parse(localStorage.getItem("cart"))
   cartTableBody = document.querySelector("#cartTableBody")
 
   //Iga toote kohta tehakse rida html tbody elementi
   for (let index = 0; index < cartJson.length; index++) {
-    const cartRow = cartJson[index];
-  
+    const cartRow = cartJson[index]
+
     let tdName = document.createElement("td")
     tdName.textContent = cartRow["name"]
-  
+
     let tdPrice = document.createElement("td")
     tdPrice.textContent = cartRow["price"] + " €"
-  
+
     let Button = document.createElement("button")
     Button.textContent = "X"
     Button.onclick = removeProductFromCart
-  
+
     let tr = document.createElement("tr")
     tr.appendChild(tdName)
     tr.appendChild(tdPrice)
@@ -44,15 +44,14 @@ function createCartTable(){
   }
 }
 
-
-function createSummary(){
+function createSummary() {
   let cartJson = JSON.parse(localStorage.getItem("cart"))
   let cartPriceSum = 0
   for (let index = 0; index < cartJson.length; index++) {
-    const cartRow = cartJson[index];
+    const cartRow = cartJson[index]
     cartPriceSum += cartRow["price"]
   }
-  cartPriceSum = Math.round(cartPriceSum*10)/10
+  cartPriceSum = Math.round(cartPriceSum * 10) / 10
   //lisa uued väärtused ka html elementidele
   document.getElementById("subtotal").textContent = cartPriceSum + " €"
   document.getElementById("shippingEstimate").textContent = 5 + " €"
@@ -61,3 +60,13 @@ function createSummary(){
 
 createCartTable()
 createSummary()
+
+// Bruno Hiis, listenime checkout nupule
+document.getElementById("checkout").onclick = () => {
+  // Bruno Hiis, kui ostukorv on tühi, siis ei saa edasi midagi teha
+  if (JSON.parse(localStorage.getItem("cart")).length === 0) {
+    alert("Your cart is empty")
+  } else {
+    alert("Thank you for your purchase!")
+  }
+}
--- js/product.js
+++ js/product.js
@@ -1,42 +1,55 @@
-import hiirData from '../data.json' assert {type: 'json'};
+import hiirData from "../data.json" assert { type: "json" }
 
 //Kaarel Tamuri - Returnib div-i, kus on andmed data.json failist
-function hiirTemplate(hiir) { 
+function hiirTemplate(hiir) {
   return `
     <div class="product">
         <img class="hiir-photo" src="${hiir.photo}">
         <h2 class="hiir-name">${hiir.name} </h2>
-        <p><strong></strong> ${hiir.price}</p>
+        <p><strong></strong> ${hiir.price} €</p>
 
         <button class = "buyButton">Add to cart</button>
+        <div id="${hiir.name}-message-wrapper"></div>
     </div>
-  `;
+  `
 }
 
 //Kaarel Tamuri - käib kõik objektid hiirData arrays läbi ning rakendab iga ühe jaoks hiirData funktsiooni, joinib need kokku ning lisab id="app" kohale products.html failis
 document.getElementById("app").innerHTML = `
     ${hiirData.map(hiirTemplate).join("")}
     
-`;
-
+`
 
 // Raido Aunpuu, lisab nuppudele onClick, et asju ostukorvi lisada
-document.getElementById("app").querySelectorAll(".buyButton").forEach((btn)=>{
-  btn.onclick = (event)=>{
-    let productName = event.target.parentElement.querySelector('.hiir-name').innerText
-    for (let index = 0; index < hiirData.length; index++) {
-      const product = hiirData[index];
-      if (product.name === productName){
-        index = hiirData.length
-        let cartJson = JSON.parse(localStorage.getItem("cart"))
-        //kui localstorage on tühi
-        if (cartJson === null){
-          cartJson = []
+document
+  .getElementById("app")
+  .querySelectorAll(".buyButton")
+  .forEach((btn) => {
+    btn.onclick = (event) => {
+      let productName = event.target.parentElement.querySelector(".hiir-name").innerText
+      for (let index = 0; index < hiirData.length; index++) {
+        const product = hiirData[index]
+        if (product.name === productName) {
+          index = hiirData.length
+          let cartJson = JSON.parse(localStorage.getItem("cart"))
+          //kui localstorage on tühi
+          if (cartJson === null) {
+            cartJson = []
+          }
+          cartJson.push(product)
+          localStorage.setItem("cart", JSON.stringify(cartJson))
+
+          // Bruno Hiis, võtame ostukorvi lisamise teateks vajaliku data
+          const howmanyThisItermInCart = cartJson.filter(
+            (item) => item.name === productName
+          )
+          const successElementId = `${product.name}-message-wrapper`
+
+          // Bruno Hiis, näitame kasutajale, et toode on lisatud ostukorvi
+          document.getElementById(successElementId).innerHTML =
+            "Product added to cart" + " " + howmanyThisItermInCart.length + "x"
+          document.getElementById(successElementId).className = "success-message"
         }
-        cartJson.push(product)
-        localStorage.setItem("cart",JSON.stringify(cartJson))
       }
     }
-  }
-}) 
-
+  })
